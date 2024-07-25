93ae85dc32f789b8d12ebd9110578411b9132858
PhilipDeegan <philip.deegan@gmail.com>
2024-07-25 22:27:49 +0200

    cholesky build python3 eg

--- cholesky/init.sh
+++ cholesky/init.sh
@@ -1,3 +1,38 @@
 #!/usr/bin/env bash
+## Script to configure and build phare with one thread
 
-# not tested yet
+set -ex
+set -o pipefail
+shopt -s expand_aliases
+
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" && cd "$CWD"
+
+alias glog="git log --pretty=oneline --abbrev-commit"
+alias cls="clear; printf '\033[3J'"
+
+module load cmake gcc/10.2.0 openmpi hdf5
+
+cd "$HOME"
+[ ! -d "PHARE" ] && git clone https://github.com/PHAREHUB/PHARE --recursive
+
+cd PHARE
+[ ! -d ".venv" ] && /mnt/beegfs/softs/opt/core/python-envs/lspython/bin/python3 -m venv .venv
+[ ! -f ".venv/bin/activate" ] && echo "error: venv not working as expected" && exit 1
+
+# shellcheck disable=SC1091
+. .venv/bin/activate
+[ ! -f ".pip_installed" ] && python3 -m pip install -r requirements.txt && echo 1 >.pip_installed
+
+export PYTHONPATH="${WORKDIR}/build:${PWD}:${PWD}/pyphare"
+
+cd "$WORKDIR"
+
+# write script if missing - phare_configurator crashes with some ucx assertion
+[ ! -f "build.sh" ] && cat >build.sh <<EOL
+mkdir -p build && cd build
+CMAKE_CXX_FLAGS="-DNDEBUG -g0 -O3 -march=native -mtune=native" # -DPHARE_DIAG_DOUBLES=1
+cmake ~/PHARE/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="\${CMAKE_CXX_FLAGS}" -Dphare_configurator=OFF
+make
+EOL
+chmod +x build.sh
+[ ! -d "build" ] && ./build.sh
