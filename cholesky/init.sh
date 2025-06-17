4817c32383c48afaefafbea7ed58d6e1151f433f
deegan <philip.deegan@gmail.com>
2025-06-17 11:26:26 +0200

    use python3.11 on cholesky

--- cholesky/init.sh
+++ cholesky/init.sh
@@ -3,12 +3,10 @@
 
 set -ex
 set -o pipefail
-shopt -s expand_aliases
 
-alias glog="git log --pretty=oneline --abbrev-commit"
-alias cls="clear; printf '\033[3J'"
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" && cd "$CWD"
 
-module load cmake gcc/10.2.0 openmpi hdf5
+module load cmake gcc/13.2.0 openmpi hdf5
 
 cd "$HOME"
 [ ! -d "PHARE" ] && git clone https://github.com/PHAREHUB/PHARE --recursive
@@ -21,9 +19,7 @@ cd PHARE
 . .venv/bin/activate
 [ ! -f ".pip_installed" ] && python3 -m pip install -r requirements.txt && echo 1 >.pip_installed
 
-export PYTHONPATH="${WORKDIR}/build:${PWD}:${PWD}/pyphare"
-
-cd "$WORKDIR"
+export PYTHONPATH="${PWD}/build:${PWD}:${PWD}/pyphare"
 
 # write script if missing - phare_configurator crashes with some ucx assertion
 [ ! -f "build.sh" ] && cat >build.sh <<EOL
---
+++ cholesky/job.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# some links if needed
+# https://docs.idcs.mesocentre.ip-paris.fr/cholesky/slurm_job_management
+# https://docs.idcs.mesocentre.ip-paris.fr/cholesky/slurm_queues_description/
+
+## BEGIN SBATCH directives
+#SBATCH --job-name=run052a
+#SBATCH --output=run052a.txt
+#
+#SBATCH --ntasks=40
+#SBATCH --nodes=1
+#SBATCH --time=10:00:00
+#SBATCH --partition=cpu_shared
+#SBATCH --account=phare
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=your@lpp.polytechnique.fr
+## END SBATCH directives
+
+module load cmake gcc/13.2.0 openmpi hdf5
+
+cd $HOME/PHARE
+. .venv/bin/activate
+export PYTHONPATH="${PWD}/build:${PWD}:${PWD}/pyphare"
+cd ..
+
+PRELOAD="/mnt/beegfs/softs/opt/core/gcc/13.2.0/lib64/libstdc++.so"
+export LD_PRELOAD="${PRELOAD}"
+mpirun -n "$SLURM_NTASKS" python3 -O harris.py
