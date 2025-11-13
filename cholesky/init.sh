cbec66b185e15d857e15e83c0877d611d7a9c640
PhilipDeegan <philip.deegan@gmail.com>
2025-11-13 18:17:11 +0100

    ++

--- cholesky/init.sh
+++ cholesky/init.sh
@@ -5,15 +5,16 @@ set -ex
 set -o pipefail
 
 CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" && cd "$CWD"
+WORKDIR="/mnt/beegfs/workdir/$(whoami)" # not in env sometimes
 
-module load cmake gcc/13.2.0 openmpi hdf5
+module load cmake gcc/13.2.0 openmpi hdf5 git/2.31.1
 
 # needed during pip install for some reason
 export CC=/mnt/beegfs/softs/opt/core/gcc/13.2.0/bin/gcc
 export CXX=/mnt/beegfs/softs/opt/core/gcc/13.2.0/bin/g++
 
-cd "$HOME"
-[ ! -d "PHARE" ] && git clone https://github.com/PHAREHUB/PHARE --recursive
+cd "$WORKDIR"
+[ ! -d "PHARE" ] && git clone https://github.com/PhilipDeegan/PHARE -b 3d_new --recursive --depth 10 --shallow-submodules
 
 cd PHARE
 [ ! -d ".venv" ] && /mnt/beegfs/softs/opt/core/python-envs/lspython/bin/python3 -m venv .venv
@@ -25,12 +26,18 @@ cd PHARE
 
 export PYTHONPATH="${PWD}/build:${PWD}:${PWD}/pyphare"
 
-# write script if missing - phare_configurator crashes with some ucx assertion
+cd ..
+
+# write script if missing
 [ ! -f "build.sh" ] && cat >build.sh <<EOL
-mkdir -p build && cd build
-CMAKE_CXX_FLAGS="-DNDEBUG -g0 -O3 -march=native -mtune=native" # -DPHARE_DIAG_DOUBLES=1
-cmake ~/PHARE/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="\${CMAKE_CXX_FLAGS}" -Dphare_configurator=OFF
-make
+cd PHARE
+mkdir -p build
+cd build
+CMAKE_CXX_FLAGS="-DNDEBUG -g0 -O3 -march=native -mtune=native  -DPHARE_DIAG_DOUBLES=1"
+cmake .. -Dtest=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="\${CMAKE_CXX_FLAGS}"
+make -j 10
 EOL
 chmod +x build.sh
-[ ! -d "build" ] && ./build.sh
+
+# shellcheck disable=SC2015
+[ ! -d "build" ] && ./build.sh || true
--- tgcc/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-# not tested yet
